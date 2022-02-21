daebf83e5d8be2643a279e7681a716ee296788ec
Gabriel Maximo <gbielmaximo@gmail.com>
2022-02-21 20:13:40 -0300

    separando a bagaça

---
+++ util/checkSpam/addMessage.js
@@ -0,0 +1,17 @@
+import redis from "../../db/db.js"
+import Constants from "../Constants.js"
+
+export default async function addMessage(message) {
+   const haveMessage = await redis.json.get(Constants.MESSAGES)
+   if (haveMessage[message.content] === undefined) {
+      // salva la
+      const data = { id: message.id, timestamp: message.createdTimestamp }
+      redis.json.set(Constants.MESSAGES, `.${message.content}`, {messages: [data]})
+      return
+   }
+   
+   const data = { id: message.id, timestamp: message.createdTimestamp }
+   redis.json.arrAppend(Constants.MESSAGES, `.${message.content}.messages`, data)
+
+   return redis.json.get(Constants.MESSAGES, `.${message.content}.messages`)
+}
--- util/checkSpam/checkMessage.js
+++ util/checkSpam/checkMessage.js
@@ -1,18 +1,7 @@
-import { Client, Message } from "discord.js"
-import redis from "../../db/db.js"
-import Constants from "../Constants.js"
+import addMessage from "./addMessage.js"
 
-export default async function checkMessage(message, client) {
-   const haveMessage = await redis.json.get(Constants.MESSAGES)
-   console.log("recebido: ", haveMessage[message.content])
-   if (haveMessage[message.content] === undefined) {
-      // salva la
-      const data = { id: message.id, timestamp: message.createdTimestamp }
-      console.log("criando: ", data)
-      redis.json.set(Constants.MESSAGES, `.${message.content}`, {messages: [data]})
-      return
-   }
-   
-   const data = { id: message.id, timestamp: message.createdTimestamp }
-   redis.json.arrAppend(Constants.MESSAGES, `.${message.content}.messages`, data)
+
+export default async function checkMessage(message) {
+   const array = await addMessage(message)
+   console.log(array)
 }
