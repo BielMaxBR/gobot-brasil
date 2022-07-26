edd93aa3cb2a173935e75cecbea224e1cc4495c5
gbielmaximo@gmail.com <gbielmaximo@gmail.com>
2022-07-26 19:04:10 -0300

    erro 1

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -6,6 +6,7 @@ export default class InteractionCreate {
     }
 
     async run(interaction) {
+        console.log(interaction instanceof CommandInteraction)
         switch (interaction.constructor) {
             case CommandInteraction:
                 try {
