66340670ef07b4f332a434d93fe067660225c311
matheus arruda <59703897+matheus-s-arruda@users.noreply.github.com>
2022-08-03 18:30:29 -0300

    Update messageCreate.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -12,6 +12,7 @@ class Message {
     async run(message) {
         if (message.author.bot) return
         if (message.content == "vasco") message.reply("DEVOLVA")
+        if (message.content == "diz pra ele bot") message.reply("PELO AMOR DE DEUS, LEIA A DOCUMENTAÇÃO!!!")
         const isSpam = await checkMessage(message, message.guild.id, message.channel.id)
         if (isSpam) deleteSpam(message, message.guild)
     }
