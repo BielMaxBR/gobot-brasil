bc176ff4ee11451d21ec7ed1f2278aca41f7d786
Gabriel Maximo <gbielmaximo@gmail.com>
2022-11-21 18:01:05 +0000

    temos o código!

--- Commands/util/compile.js
+++ Commands/util/compile.js
@@ -1,5 +1,6 @@
 import { exec } from 'child_process'
 import { EmbedBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle } from 'discord.js'
+import { readFile, writeFile } from 'fs/promises'
 import Command from '../../classes/Command.js'
 
 class Compile extends Command {
@@ -40,8 +41,44 @@ class Compile extends Command {
         }
     }
 
-    runForMessage(message, client, args) {
-        console.log(args)
+    async runForMessage(message, client, args) {
+        if (args[0].startsWith("script")) {
+            console.log(args)
+            args[0] = args[0].replace("script", "")
+            let script = args
+                .join(" ")
+                .replaceAll("```", "")
+                .replace("swift", "")
+                .trim()
+
+            let newFile = "segredo gigatônico"
+            if (!(script.includes("extends") || script.includes("func _init():"))) {
+                let file = await readFile("default-script.gd", "utf8")
+                console.log(file)
+                file = file.replace(":INSERIR:", script)
+                console.log(file)
+                newFile = await writeFile('temp-script.gd', file)
+            } else {
+                newFile = await writeFile('temp-script.gd', script)
+            }
+            if (newFile === "segredo gigatônico") return
+
+            exec("chmod +x godot.64 && ./godot.64 -s temp-script.gd", (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                const embed = new EmbedBuilder()
+                    .setTitle("Saída do script")
+                    .setDescription("```\n" + stdout + "```")
+                message.channel.send({embeds:[embed]})
+            })
+        }
     }
 
     help(interaction) {
